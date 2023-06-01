b5e2b0452bd614f9cc94f6c68cf3bc56877e0966
rafikusdiarto <rafikusdiarto1707@gmail.com>
2023-06-02 00:45:10 +0700

    add:new learning path page members

--- application/views/users/layouts.php
+++ application/views/users/layouts.php
@@ -24,6 +24,192 @@
 
     <!-- Custom styles for this template-->
     <link href="<?=base_url()?>assets/css/sb-admin-2.min.css" rel="stylesheet">
+	<style>
+		/* hero */
+		h1, h2, h3, h4, h5, h6 { color: #25292a; margin: 0px 0px 10px 0px; font-family: 'Overpass', sans-serif; font-weight: 700; letter-spacing: -1px; line-height: 1; }
+		a { text-decoration: none; color: #8d8f90; -webkit-transition: all 0.3s; -moz-transition: all 0.3s; transition: all 0.3s; }
+		a:focus, a:hover { text-decoration: none; color: #f85759; }
+
+		.page-header { background: url(https://easetemplate.com/free-website-templates/hike/images/pageheader.jpg)no-repeat; position: relative; background-size: cover; }
+		.page-caption { padding-top: 170px; padding-bottom: 174px; }
+		.page-title { font-size: 46px; line-height: 1; color: #fff; font-weight: 600; text-align: center; }
+
+		.card-section { position: relative; bottom: 60px; }
+		.card-block { padding: 80px; }
+		.section-title { margin-bottom: 60px; }
+		
+		/* card */
+		.section_our_solution .row {
+		align-items: center;
+		margin-top:100px;
+		}
+
+		.our_solution_category {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		}
+		.our_solution_category .solution_cards_box {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		}
+		.solution_cards_box .solution_card {
+		flex: 0 50%;
+		background: #fff;
+		box-shadow: 0 2px 4px 0 rgba(136, 144, 195, 0.2),
+			0 5px 15px 0 rgba(37, 44, 97, 0.15);
+		border-radius: 15px;
+		margin: 8px;
+		padding: 10px 15px;
+		position: relative;
+		z-index: 1;
+		overflow: hidden;
+		min-height: 265px;
+		transition: 0.7s;
+		}
+
+		.solution_cards_box .solution_card:hover {
+		background: #309df0;
+		color: #fff;
+		transform: scale(1.1);
+		z-index: 9;
+		}
+
+		.solution_cards_box .solution_card:hover::before {
+		background: rgb(85 108 214 / 10%);
+		}
+
+		.solution_cards_box .solution_card:hover .solu_title h3,
+		.solution_cards_box .solution_card:hover .solu_description p {
+		color: #fff;
+		}
+
+		.solution_cards_box .solution_card:before {
+		content: "";
+		position: absolute;
+		background: rgb(85 108 214 / 5%);
+		width: 170px;
+		height: 400px;
+		z-index: -1;
+		transform: rotate(42deg);
+		right: -56px;
+		top: -23px;
+		border-radius: 35px;
+		}
+
+		.solution_cards_box .solution_card:hover .solu_description button {
+		background: #fff !important;
+		color: #309df0;
+		}
+
+		.solution_card .so_top_icon {
+		}
+
+		.solution_card .solu_title h3 {
+		color: #212121;
+		font-size: 1.3rem;
+		margin-top: 13px;
+		margin-bottom: 13px;
+		}
+
+		.solution_card .solu_description p {
+		font-size: 15px;
+		margin-bottom: 15px;
+		}
+
+		.solution_card .solu_description button {
+		border: 0;
+		border-radius: 15px;
+		background: linear-gradient(
+			140deg,
+			#42c3ca 0%,
+			#42c3ca 50%,
+			#42c3cac7 75%
+		) !important;
+		color: #fff;
+		font-weight: 500;
+		font-size: 1rem;
+		padding: 5px 16px;
+		}
+
+		.our_solution_content h1 {
+		text-transform: capitalize;
+		margin-bottom: 1rem;
+		font-size: 2.5rem;
+		}
+		.our_solution_content p {
+		}
+
+		.hover_color_bubble {
+		position: absolute;
+		background: rgb(54 81 207 / 15%);
+		width: 100rem;
+		height: 100rem;
+		left: 0;
+		right: 0;
+		z-index: -1;
+		top: 16rem;
+		border-radius: 50%;
+		transform: rotate(-36deg);
+		left: -18rem;
+		transition: 0.7s;
+		}
+
+		.solution_cards_box .solution_card:hover .hover_color_bubble {
+		top: 0rem;
+		}
+
+		.solution_cards_box .solution_card .so_top_icon {
+		width: 60px;
+		height: 60px;
+		border-radius: 50%;
+		background: #fff;
+		overflow: hidden;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		}
+
+		.solution_cards_box .solution_card .so_top_icon img {
+		width: 40px;
+		height: 50px;
+		object-fit: contain;
+		}
+
+		/*start media query*/
+		@media screen and (min-width: 320px) {
+		.sol_card_top_3 {
+			position: relative;
+			top: 0;
+		}
+
+		.our_solution_category {
+			width: 100%;
+			margin: 0 auto;
+		}
+
+		.our_solution_category .solution_cards_box {
+			flex: auto;
+		}
+		}
+		@media only screen and (min-width: 768px) {
+		.our_solution_category .solution_cards_box {
+			flex: 1;
+		}
+		}
+		@media only screen and (min-width: 1024px) {
+		.sol_card_top_3 {
+			position: relative;
+			top: -3rem;
+		}
+		.our_solution_category {
+			/* width: 50%; */
+			margin: 0 auto;
+		}
+		}
+
+	</style>
 </head>
 
 <body id="page-top">
@@ -32,7 +218,6 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <?php echo $sidebar; ?>
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
@@ -45,10 +230,9 @@
                 <?php echo $navbar; ?>
                 <!-- End of Topbar -->
 				<!-- /.container-fluid -->
-				<div class="container-fluid">
-						<?php $this->load->view($content_view); ?>
-
-					</div>
+				<div class="">
+					<?php $this->load->view($content_view); ?>
+				</div>
 
             </div>
             <!-- End of Main Content -->
