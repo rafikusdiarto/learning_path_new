1c49268dbd02a2fb5b7751e8d1dea6a4437df149
rafikusdiarto <rafikusdiarto1707@gmail.com>
2023-06-07 21:38:54 +0700

    fix:sidebar admin

--- application/views/admin/layouts/components/sidebar.php
+++ application/views/admin/layouts/components/sidebar.php
@@ -1,11 +1,11 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url('admin')?>">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url('admin/dashboard')?>">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Learning Path<sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">Learning Path</div>
             </a>
 
             <!-- Divider -->
